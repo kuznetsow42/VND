3321bfb40a6ec64a36f1af3c834cfe15757c101c
andrey <kuznetsow42@gmail.com>
2023-11-04 18:25:11 +0500

    tests: add status tests in TestPermissions(TestEnginePermissions) class

--- vnd/users/tests.py
+++ vnd/users/tests.py
@@ -1,8 +1,10 @@
+from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK, HTTP_204_NO_CONTENT, HTTP_201_CREATED
 from rest_framework_simplejwt.tokens import AccessToken
 import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
-from users.models import CustomUser, Engine
+from users.models import CustomUser, Engine, Status
+
 
 @pytest.fixture
 def client():
@@ -28,9 +30,15 @@ def user():
 def token(user):
     return str(AccessToken.for_user(user))
 
+
 @pytest.fixture
 def engine():
     return Engine.objects.create(name="Test", links={"test": "test"}, description="dsadsafeqwas")
+
+
+@pytest.fixture
+def status():
+    return Status.objects.create(name="Test")
 
 
 @pytest.mark.django_db
@@ -38,14 +46,14 @@ class TestAuthentication:
     def test_user_registration(self, user_data, client):
         path = "/api/v1/users/register/"
         response = client.post(path, user_data, format="json")
-        assert response.status_code == status.HTTP_201_CREATED
+        assert response.status_code == HTTP_201_CREATED
         assert "password" not in response.data
         assert CustomUser.objects.count() == 1
 
     def test_user_login(self, user, client):
         path = "/api/v1/users/login/"
         response = client.post(path, {"username": "testuser", "password": "123wedsadweq"}, format="json")
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == HTTP_200_OK
         assert set(response.data.keys()) == {"access", "refresh", "username", "avatar"}
 
 
@@ -54,39 +62,42 @@ class TestUsersViews:
     def test_user_list(self, user, client):
         path = "/api/v1/users/"
         response = client.get(path)
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == HTTP_200_OK
         assert response.data[0]["username"] == "testuser"
 
     def test_user_detail(self, user, token, client):
         path = "/api/v1/users/detail/"
         response = client.get(path)
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.status_code == HTTP_401_UNAUTHORIZED
         client.credentials(HTTP_AUTHORIZATION="Bearer " + token)
         response = client.get(path)
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == HTTP_200_OK
         assert response.data["username"] == "testuser"
         response = client.delete(path)
-        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert response.status_code == HTTP_204_NO_CONTENT
         assert CustomUser.objects.count() == 0
 
 
 @pytest.mark.django_db
-class TestEnginePermissions:
+class TestPermissions:
 
     def test_get(self, engine, client):
-        path = "/api/v1/users/engines/"
-        response = client.get(path)
-        assert response.status_code == status.HTTP_200_OK
-        assert response.data[0]["name"] == "Test"
+        path = "/api/v1/users/"
+        response = client.get(path + "engines/")
+        assert response.status_code == HTTP_200_OK
+        response = client.get(path + "statuses/")
+        assert response.status_code == HTTP_200_OK
 
     def test_create(self, client):
-        path = "/api/v1/users/engines/"
-        response = client.post(path)
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        path = "/api/v1/users/"
+        response = client.post(path + "engines/")
+        assert response.status_code == HTTP_401_UNAUTHORIZED
+        response = client.post(path + "statuses/")
+        assert response.status_code == HTTP_401_UNAUTHORIZED
 
-    def test_delete(self, engine, client):
-        path = f"/api/v1/users/engines/{engine.id}/"
-        response = client.delete(path)
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
+    def test_delete(self, engine, status, client):
+        path = f"/api/v1/users/"
+        response = client.delete(path + f"engines/{engine.id}/")
+        assert response.status_code == HTTP_401_UNAUTHORIZED
+        response = client.delete(path + f"engines/{id}/")
+        assert response.status_code == HTTP_401_UNAUTHORIZED
