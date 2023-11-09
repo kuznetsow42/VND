b4ec160d69fec681e7219e48cba014be0f1c91f6
andrey <kuznetsow42@gmail.com>
2023-11-09 11:03:21 +0500

    feat: specify logic for updating engines filed in UserDetailView

--- vnd/users/views.py
+++ vnd/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from users.models import CustomUser
@@ -23,5 +24,19 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     def get_object(self):
         return CustomUser.objects.get(pk=self.request.user.pk)
 
+    def patch(self, request, *args, **kwargs):
+        data = request.data
+        if "favorite_engines" not in data:
+            super().patch(request, *args, **kwargs)
+        else:
+            user = self.get_object()
+            serializer = self.get_serializer(user, data=data, partial=True)
+            if data["action"] == "delete":
+                user.favorite_engines.remove(data["favorite_engines"])
+            else:
+                user.favorite_engines.add(data["favorite_engines"])
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(serializer.data)
 
 
